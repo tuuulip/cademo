6ee452d3eb2f9f0442370e34d95d6931d2c881e8
stephenwu <stephenwu2020@protonmail.com>
2022-05-13 16:06:30 +0800

    config settings

--- caserver/server.go
+++ caserver/server.go
@@ -2,23 +2,27 @@ package caserver
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
+	"cademo/config"
 
 	"github.com/hyperledger/fabric-ca/lib"
 )
 
 func GetServer() *lib.Server {
-	homeDir := "/Users/stephen/develop/gotut/cademo/cahome"
+	homeDir := getCaHome()
 	dbsrc := fmt.Sprintf(
 		"host=%s port=5432 user=%s password=%s dbname=%s sslmode=%s",
-		"10.10.7.35",
-		"root",
-		"baas123",
-		"fabric_ca_zoo",
-		"disable",
+		config.Configer.GetString("cadb.host"),
+		config.Configer.GetString("cadb.user"),
+		config.Configer.GetString("cadb.password"),
+		config.Configer.GetString("cadb.dbname"),
+		config.Configer.GetString("cadb.sslmode"),
 	)
 	id := lib.CAConfigIdentity{
-		Name:           "admin",
-		Pass:           "adminpw",
+		Name:           config.Configer.GetString("caadmin.user"),
+		Pass:           config.Configer.GetString("caadmin.pass"),
 		MaxEnrollments: -1,
 	}
 	db := lib.CAConfigDB{
@@ -44,4 +48,12 @@ func GetServer() *lib.Server {
 			ConfigFilePath: homeDir,
 		},
 	}
+}
+
+func getCaHome() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(pwd, config.Configer.GetString("cahome"))
 }
